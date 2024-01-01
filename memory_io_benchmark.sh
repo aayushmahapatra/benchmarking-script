a5d8a49fc5ba32babf6a8062c7da409c932943fb
Aayush <mahapatraaayush@gmail.com>
2024-01-02 03:57:59 +0530

    Update memory_io_benchmark.sh

--- memory_io_benchmark.sh
+++ memory_io_benchmark.sh
@@ -3,13 +3,13 @@
 # Function to perform memory read benchmark
 memory_read_benchmark() {
     echo "Running memory read benchmark..."
-    stream -v 1 -t 4
+    sysbench --test=memory --memory-oper=read run
 }
 
 # Function to perform memory write benchmark
 memory_write_benchmark() {
     echo "Running memory write benchmark..."
-    stream -v 2 -t 4
+    sysbench --test=memory --memory-oper=write run
 }
 
 # Run benchmarks
